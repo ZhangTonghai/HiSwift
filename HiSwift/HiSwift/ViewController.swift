6fe9a77f42b50256d2d36b61831fb8b18e32a4bc
ZhangTonghai <tonghaizhang@gmail.com>
2018-03-21 14:08:31 +0800

    HiSwift init

---
+++ HiSwift/HiSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiSwift
+//
+//  Created by Haizi on 2018/3/19.
+//  Copyright © 2018年 Hi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
